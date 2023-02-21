8ed3d02ba377ac574aaa1d62c6ac1569d42c17a5
Peter-Mascherin <pe.mascherin@gmail.com>
2023-02-20 21:29:52 -0500

    implemented global hotkey, virtual clicking with start and stop, and partial styles

--- PetersAutoClicker/MainWindow.xaml.cs
+++ PetersAutoClicker/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,34 +13,139 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Forms;
+using System.Windows.Interop;
 
 namespace PetersAutoClicker
 {
    //We figured it out, we can now develop an autoclicker
     public partial class MainWindow : Window
     {
+        //i will use the F8 key on my keyboard
+
+        [DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
+
+        [DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        
+        
+        //keystate
+        private bool hotkeystate = false;
+
+        //main key i want (tested and this works as well)
+        private uint mykey = (uint)Keys.F8.GetHashCode();
+
+        // the window source used to setup the global hotkey
+        private HwndSource _source;
+
+        //an id to track the hotkey
+        const int HOTKEY_ID = 9000;
+
+        //MouseHandler class
+        public MouseHandler mouseObject;
+
+
         public MainWindow()
         {
             InitializeComponent();
+            
         }
 
-        private void ChangeLabelColour(object sender, RoutedEventArgs e)
+
+        //when app is initialized, creates the hook from the window and calls our RegisterHotKey overload method
+        protected override void OnSourceInitialized(EventArgs e)
         {
-            thelabel.Foreground = Brushes.SteelBlue;
+            base.OnSourceInitialized(e);
+            var helper = new WindowInteropHelper(this);
+            mouseObject = new MouseHandler();
+            _source = HwndSource.FromHwnd(helper.Handle);
+            _source.AddHook(HwndHook);
+            RegisterHotKey();
+            
         }
 
-        private void mainbtn_Click(object sender, RoutedEventArgs e)
+        //cleanup method for closing the app, removes hook and nulls the window source, unregisters hotkey then closes
+        protected override void OnClosed(EventArgs e)
         {
-            anotherbtn.Foreground = Brushes.White;
-            thelabel.Content = "Main Button clicked me";
-            thelabel.Foreground = mainbtn.Background;
+            _source.RemoveHook(HwndHook);
+            _source = null;
+            UnregisterHotKey();
+            base.OnClosed(e);
         }
 
-        private void anotherbtn_Click(object sender, RoutedEventArgs e)
+
+        //our overload method, simply calls a helper to get window handle and registers hotkey with a label indicating whether it was registered or not
+        private void RegisterHotKey()
         {
-            thelabel.Content = "Another Button clicked me";
-            thelabel.Foreground = Brushes.Gold;
-            mainbtn.Background = Brushes.RosyBrown;
+            var helper = new WindowInteropHelper(this);
+            if(!RegisterHotKey(helper.Handle,HOTKEY_ID,0,mykey))
+            {
+                reglabel.Content = "Hotkey was not able to be registered!";
+            }
+            else
+            {
+                reglabel.Content = "hotkey was able to be registered!";
+            }
         }
+
+
+        //unregisters the hotkey
+        private void UnregisterHotKey()
+        {
+            var helper = new WindowInteropHelper(this);
+            UnregisterHotKey(helper.Handle, HOTKEY_ID);
+        }
+
+
+        //our Hook, essentially a listener that checks if whatever key was pressed matches our registered hotkey, then performs the action if true
+        private IntPtr HwndHook(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            const int WM_HOTKEY = 0x0312;
+
+            switch(msg)
+            {
+                case WM_HOTKEY:
+                    switch(wParam.ToInt32())
+                    {
+                        case HOTKEY_ID:
+                            OnHotKeyPressed();
+                            handled= true;
+                            break;
+                    }
+                    break;
+            }
+
+            return IntPtr.Zero;
+        }
+
+        private async void OnHotKeyPressed()
+        {
+            //in ms
+            var delay = 10;
+            if(hotkeystate)
+            {
+                hotkeystate= false;
+                statelabel.Content = "Hotkey is in OFF state";
+            }    
+            else
+            {
+                hotkeystate = true;
+                statelabel.Content = "Hotkey is in ON state";
+                
+                while(hotkeystate)
+                {
+                    mouseObject.mouseLeftClick();
+                    await Task.Delay(delay);
+                }
+                
+                
+            }
+            
+            
+        }
+
+        
     }
 }
---
+++ PetersAutoClicker/MouseHandler.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PetersAutoClicker
+{
+    public class MouseHandler
+    {
+        public MouseHandler() 
+        { 
+
+        }
+
+        [DllImport("user32.dll")]
+        static extern void mouse_event(int dwFlags, int dx, int dy,int dwData, int dwExtraInfo);
+
+        [Flags]
+        public enum MouseEventFlags
+        {
+            LEFTDOWN = 0x00000002,
+            LEFTUP = 0x00000004,
+            MIDDLEDOWN = 0x00000020,
+            MIDDLEUP = 0x00000040,
+            MOVE = 0x00000001,
+            ABSOLUTE = 0x00008000,
+            RIGHTDOWN = 0x00000008,
+            RIGHTUP = 0x00000010
+        }
+
+        public void mouseLeftClick()
+        {
+            var cursor_x = Cursor.Position.X;
+            var cursor_y = Cursor.Position.Y;
+            mouse_event((int)MouseEventFlags.LEFTDOWN, cursor_x, cursor_y, 0, 0);
+            mouse_event((int)MouseEventFlags.LEFTUP,cursor_x,cursor_y, 0, 0);
+        }
+    }
+}
